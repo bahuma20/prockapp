ab09fc4860c9d0d1a0710fbad80b2fab7cb5a7c6
Max Bachhuber <max.bachhuber@bahuma.io>
2020-11-07 17:43:45 +0100

    Add capacitor and store authentication data in local storage.

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {Plugins, StoragePlugin} from '@capacitor/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  storage: StoragePlugin;
+
+  constructor(private http: HttpClient) {
+    this.storage = Plugins.Storage;
+  }
 
   login(email: string, password: string): Observable<any> {
     return new Observable<any>(observer => {
@@ -17,13 +22,51 @@ export class AuthService {
           password
         },
       }).subscribe((data: any) => {
-        console.log('finished');
-        console.log(data);
-        observer.next();
-      }, error => {
+        console.log('LoggedIn');
+
+        Promise.all([
+          this.storage.set({
+            key: 'auth_email',
+            value: email,
+          }),
+          this.storage.set({
+            key: 'auth_password',
+            value: password,
+          })
+        ]).then(() => {
+          observer.next();
+        })
+
+        }, error => {
         console.error(error);
         observer.error(error);
       })
+    });
+  }
+
+  authenticate(): Observable<any> {
+    return new Observable<any>(observer => {
+      Promise.all([
+        this.storage.get({
+          key: 'auth_email',
+        }),
+        this.storage.get({
+          key: 'auth_password',
+        }),
+      ]).then(values => {
+        const [email, password] = values;
+
+        this.http.post('http://localhost:3001/user/login', {
+          data: {
+            email,
+            password
+          },
+        }).subscribe((data: any) => {
+          observer.next();
+        }, error => {
+          observer.error(error);
+        });
+      });
     });
   }
 }
